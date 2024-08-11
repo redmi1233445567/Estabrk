6f7713ca698bce822a2ebca3b5bc9cdc5549c327
redmi <hmadaredmi7@gmail.com>
2024-08-11 19:49:35 +0300

    css2

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
-      <body className={inter.className}>
+      <body className="relative">
           <Head />
           {children}
           <Footer />
