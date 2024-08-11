763e8e2e5f2617f80673baa85a5b0f17f65ab294
redmi <hmadaredmi7@gmail.com>
2024-08-11 11:56:54 +0300

    finish

---
+++ app/components/Footer.jsx
@@ -0,0 +1,9 @@
+
+export default function Footer() {
+  return (
+    <div className="mt-7 group w-full text-center p-5 border-t-2 border-t-black relative transition-all hover:ease-in hover:text-white ">
+      <h1 className="text-2xl font-bold group-hover:opacity-100">Ahmed Sayed 2024</h1>
+      <div className="absolute h-full w-full top-0 left-0 -z-10 opacity-0 bg-black group-hover:opacity-100"></div>
+    </div>
+  )
+}
